c59e615887bce8022d1ac9e6b0a7b85825df32f7
hyga2c <hyg.a2c@gmail.com>
2021-05-12 21:04:01 +0900

    hyg original qmk setting for claw44

---
+++ keyboards/claw44/keymaps/hygmap/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MASTER_LEFT
+#define OLED_FONT_H "keyboards/claw44/lib/glcdfont.c"
+
+#define TAPPING_TERM 150
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM_PER_KEY
---
+++ keyboards/claw44/keymaps/hygmap/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE = yes
+# enable mouse key operation
+MOUSEKEY_ENABLE = yes 
+VIA_ENABLE = yes
