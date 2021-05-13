42bfb92dd242f0c60102749daf055a0c8950587f
hyga2c <hyg.a2c@gmail.com>
2021-05-13 21:00:38 +0900

    change tapping_term to 170

--- keyboards/claw44/keymaps/hygmap/config.h
+++ keyboards/claw44/keymaps/hygmap/config.h
@@ -3,6 +3,6 @@
 #define MASTER_LEFT
 #define OLED_FONT_H "keyboards/claw44/lib/glcdfont.c"
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 170
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM_PER_KEY
